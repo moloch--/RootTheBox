e02c1a6ff655e3ece1428767efd80a0c6835c3a9
haddaway <haddaway@haddaway.(none)>
2012-04-01 01:53:07 -0700

    move fixes

--- libs/CachedScores.py
+++ libs/CachedScores.py
@@ -12,7 +12,7 @@ class CachedScores():
     def add_score(self, score_update):
         ''' This should change reclaculate the memory to add the update'''
         if not self.memory.has_key(score_update.team_name):
-            self.memory[score_update.team_name] = CachedTeam(score_update.team_name)
+            self.memory[score_update.team_name] = CachedTeam(score_update)
         else:
             self.memory.get(score_update.team_name).add_score(score_update)
 
--- libs/CachedTeam.py
+++ libs/CachedTeam.py
@@ -1,9 +1,9 @@
 
 class CachedTeam():
     
-    def __init__(self, team_name):
-        self.team_name = ""
-        self.current_score = 0
+    def __init__(self, update):
+        self.team_name = update.team_name
+        self.current_score = update.value
         self.scores = []
         
     def add_score(self, score_update):
