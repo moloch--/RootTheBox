af6084f5b6b2c34baf151f69b8656fbd769bbc52
Moloch <joedemesy@gmail.com>
2012-04-18 15:53:21 -0700

    Added Apache lic to comments

--- handlers/WebsocketHandlers.py
+++ handlers/WebsocketHandlers.py
@@ -2,10 +2,24 @@
 Created on Mar 15, 2012
 
 @author: haddaway
+
+ Copyright [2012] [Redacted Labs]
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
 '''
-import logging #@UnusedImport
-import tornado.websocket #@UnresolvedImport
-from libs.SecurityDecorators import * #@UnusedWildImport
+import logging
+import tornado.websocket
+from libs.SecurityDecorators import *
 from libs.WebSocketManager import WebSocketManager
 
 class WebsocketHandler(tornado.websocket.WebSocketHandler):
--- libs/CachedScores.py
+++ libs/CachedScores.py
@@ -1,9 +1,22 @@
+'''
+ Copyright [2012] [Redacted Labs]
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+   
+'''
 
 from libs.CachedTeam import CachedTeam
 
-'''
-This is a wrapper for an entire section of the scoreboard that we will ajax in
-'''
 class CachedScores():
     
     def __init__(self):
