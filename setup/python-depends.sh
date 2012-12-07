85dd91f25b7eb68617c21ec725ba37fa5cb95a9b
moloch-- <moloch38@gmail.com>
2012-12-07 05:20:45 -0800

    Bug fix in depends script

--- setup/python-depends.sh
+++ setup/python-depends.sh
@@ -14,6 +14,8 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
+hash -r
+
 pip install --upgrade pip
 pip --upgrade virtualenv
 pip install tornado
