d9198ba28109a2aac67423829250af248c3da560
Moloch <f@ke.null>
2013-06-07 03:06:39 -0700

    Added windows version of the bot.

--- bot/build_bot.py
+++ bot/build_bot.py
@@ -31,7 +31,7 @@ import os
 sys.argv.append('py2exe')
 
 setup(
-    options={'py2exe': {'bundle_files': 1, 'compressed': 1, 'optimize': 2}},
-    windows=[{'script': 'Flag.py', 'icon_resources': [(1, 'rtb.ico')]}],
-    zipfile=None,
+    options = {'py2exe': {'bundle_files': 1, 'compressed': 1, 'optimize': 2}},
+    console = [{'script': 'bot.py', 'icon_resources': [(1, 'rtb.ico')]}],
+    zipfile = None,
 )
