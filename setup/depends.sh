9417755ba0cb14a8b7e4b48d1e53b55a670e11ec
moloch <moloch38@gmail.com>
2013-08-31 22:23:40 -0700

    Expunged all tabs from the source

--- setup/depends.sh
+++ setup/depends.sh
@@ -15,8 +15,8 @@
 #    limitations under the License.
 
 if [ "$(id -u)" != "0" ]; then
-	echo "[!] This script must be run as root." 1>&2
-	exit 1
+    echo "[!] This script must be run as root." 1>&2
+    exit 1
 fi
 
 echo "[*] Installing pip/gcc ..."
