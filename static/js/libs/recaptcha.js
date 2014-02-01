57d31d9b64c10b4aff10a7383ec29ebef941d449
moloch <moloch38@gmail.com>
2014-01-31 22:57:44 -0800

    Moved js libs around

---
+++ static/js/libs/recaptcha.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+    Recaptcha.create("6LcJJ88SAAAAAFzcmQqDPWGKRQXmJ0DCiZoPWTZf", 'recaptcha_div', {
+        theme: "blackglass",
+        callback: null
+    });
+
+});
