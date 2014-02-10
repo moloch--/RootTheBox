a70f4fab6a6a9bd588631d718a8f9eb60cd46e42
moloch <moloch38@gmail.com>
2014-02-09 23:02:37 -0800

    Tweaks to /noobalert

--- static/js/pages/public/registration.js
+++ static/js/pages/public/registration.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
     $("#handle").popover({placement:'right', trigger:'focus'});
     $("#pass1").popover({placement:'right', trigger:'focus'});
     $("#pass2").popover({placement:'right', trigger:'focus'});
-    $("#bpass1").popover({placement:'right', trigger:'focus'});
+    $("#bpass").popover({placement:'right', trigger:'focus'});
+    $("#team-name").popover({placement:'right', trigger:'focus'});
+    $("#motto").popover({placement:'right', trigger:'focus'});
     $("#regtoken").popover({placement:'right', trigger:'focus'});
 });
