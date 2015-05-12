17201b100dde002d28b1a66f5f2e48825d66a55c
moloch <moloch38@gmail.com>
2015-05-11 21:09:39 -0700

    Ported more templates to bs3, fixed popovers

--- static/js/pages/public/registration.js
+++ static/js/pages/public/registration.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-    $("#handle").popover({placement:'right', trigger:'focus'});
-    $("#pass1").popover({placement:'right', trigger:'focus'});
-    $("#pass2").popover({placement:'right', trigger:'focus'});
-    $("#bpass").popover({placement:'right', trigger:'focus'});
-    $("#team-name").popover({placement:'right', trigger:'focus'});
-    $("#motto").popover({placement:'right', trigger:'focus'});
-    $("#regtoken").popover({placement:'right', trigger:'focus'});
+    $("#handle").popover({placement:'bottom', trigger:'hover'});
+    $("#pass1").popover({placement:'bottom', trigger:'hover'});
+    $("#pass2").popover({placement:'bottom', trigger:'hover'});
+    $("#bpass").popover({placement:'bottom', trigger:'hover'});
+    $("#team-name").popover({placement:'bottom', trigger:'hover'});
+    $("#motto").popover({placement:'bottom', trigger:'hover'});
+    $("#regtoken").popover({placement:'bottom', trigger:'hover'});
 });
