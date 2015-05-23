50177aeddd70ce2a37c84b1819b1023143c16735
AnarKyx01 <anarkyx01@gmail.com>
2015-05-22 20:47:30 -0500

    Added for descriptions

---
+++ static/js/pages/admin/create/team.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+
+    /* Popovers */
+    $("#team-name").popover({placement:'right', trigger:'focus'});
+    $("#motto").popover({placement:'right', trigger:'focus'});
+
+});
