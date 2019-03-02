2f348697c3f3cfaaf4bfdc6685feaa71afe00581
ElJeffe <jeffgemail@gmail.com>
2019-03-01 22:52:29 -0500

    depends script

--- setup/depends.sh
+++ setup/depends.sh
@@ -41,13 +41,21 @@ if [[ "$SKIP" == " -y" ]]; then
   sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password your_password'
 fi
 
+python_version="$(python -c 'import platform; major, minor, patch = platform.python_version_tuple(); print(major);')"
+
 if [[ $OSTYPE == "linux-gnu" ]]; then
   echo -e "\t#########################"
   echo -e "\t   Linux Configuration"
   echo -e "\t#########################"
 
+  echo "Update package list..."
+  apt-get update
   echo "[*] Installing pip/gcc..."
-  apt-get install python-pip python-dev build-essential "$SKIP"
+  if [[ "$python_version" == "2" ]]; then
+      apt-get install python-pip python-dev build-essential "$SKIP"
+  else
+      apt-get install python3-pip python3-dev build-essential "$SKIP"
+  fi
 
   echo "[*] Installing packages..."
   apt-get install mysql-server memcached libmemcached-dev python-mysqldb python-mysqldb-dbg python-pycurl python-recaptcha zlib1g-dev libmysqlclient-dev "$SKIP"
@@ -74,7 +82,6 @@ fi
 echo "[*] Installing python libs..."
 
 #sh "$current_path/python-depends.sh"
-python_version="$(python -c 'import platform; major, minor, patch = platform.python_version_tuple(); print(major);')"
 if [[ "$python_version" == "2" ]]; then
     sh "$current_path/python2-depends.sh"
 else
