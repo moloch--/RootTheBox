db229dbbefd83aa5c391d64a12f9b7457569531e
ElJeffe <jeffgemail@gmail.com>
2020-02-18 20:11:55 -0800

    Trying bmemcached

--- libs/Sessions.py
+++ libs/Sessions.py
@@ -14,7 +14,7 @@ import logging
 import collections
 
 try:
-    import pylibmc as memcache
+    import bmemcached as memcache
 except ImportError:
     import memcache
 
@@ -225,25 +225,8 @@ def MemcachedConnect():
 
         client = memcache.Client(
             servers,
-            binary=True,
             username=user,
             password=passw,
-            behaviors={
-                # Faster IO
-                "tcp_nodelay": True,
-                # Keep connection alive
-                "tcp_keepalive": True,
-                # Timeout for set/get requests
-                "connect_timeout": 2000,  # ms
-                "send_timeout": 750 * 1000,  # us
-                "receive_timeout": 750 * 1000,  # us
-                "_poll_timeout": 2000,  # ms
-                # Better failover
-                "ketama": True,
-                "remove_failed": 1,
-                "retry_timeout": 2,
-                "dead_timeout": 30,
-            },
         )
     except TypeError:
         client = memcache.Client(options.memcached.split(","), debug=0)
