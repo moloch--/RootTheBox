7ed11857e20ea6d876144f038e474a66f3f0b7cb
ElJeffe <jeffgemail@gmail.com>
2020-02-25 20:37:11 -0800

    #345 Added feature

--- handlers/AdminHandlers/AdminUserHandlers.py
+++ handlers/AdminHandlers/AdminUserHandlers.py
@@ -47,6 +47,18 @@ class AdminManageUsersHandler(BaseHandler):
         self.render("admin/view/users.html", errors=None)
 
 
+class AdminUserStatsHandler(BaseHandler):
+    @restrict_ip_address
+    @authenticated
+    @authorized(ADMIN_PERMISSION)
+    def get(self, *args, **kwargs):
+        uuid = self.get_argument("uuid", None)
+        user = User.by_uuid(uuid)
+        print(uuid)
+        print(user)
+        self.render("admin/view/user_stats.html", user=user, errors=None)
+
+
 class AdminEditTeamsHandler(BaseHandler):
     @restrict_ip_address
     @authenticated
