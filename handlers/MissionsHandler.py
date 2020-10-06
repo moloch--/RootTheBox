8762b3924ed48d629da102af2275317725583067
ElJeffe <jeffgemail@gmail.com>
2020-10-05 21:11:56 -0400

    Fix #408 - copy / paste fatfinger

--- handlers/MissionsHandler.py
+++ handlers/MissionsHandler.py
@@ -137,7 +137,6 @@ class BoxHandler(BaseHandler):
         token = self.get_argument("token", "")
         user = self.get_current_user()
         if (box_id and Box.by_id(box_id).locked) or (
-            uuid and Flag.by_uuid(uuid).box.locked
             box_id is None and uuid and Flag.by_uuid(uuid).box.locked
         ):
             self.render(
