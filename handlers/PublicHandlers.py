de1ea05f1ffe8012e9361e3183c2fdc8a1f6e435
ElJeffe <jeffgemail@gmail.com>
2022-10-15 19:36:14 -0400

    Master branch fix for #515

--- handlers/PublicHandlers.py
+++ handlers/PublicHandlers.py
@@ -632,11 +632,13 @@ class RegistrationHandler(BaseHandler):
             account = urlsafe_b64encode(account)
             token = urlsafe_b64encode(token)
         if options.ssl:
-            origin = options.origin.replace(
-                "wss://", "https://"
-            ).replace("ws://", "https://")
+            origin = options.origin.replace("wss://", "https://").replace(
+                "ws://", "https://"
+            )
         else:
-            origin = options.origin.replace("ws://", "http://")
+            origin = options.origin.replace("wss://", "https://").replace(
+                "ws://", "http://"
+            )
         validate_url = "%s/registration/token?u=%s&t=%s" % (origin, account, token)
         remote_ip = (
             self.request.headers.get("X-Real-IP")
@@ -799,11 +801,13 @@ class ForgotPasswordHandler(BaseHandler):
             account = urlsafe_b64encode(account)
             token = urlsafe_b64encode(token)
         if options.ssl:
-            origin = options.origin.replace("ws://", "https://").replace(
-                "wss://", "https://"
+            origin = options.origin.replace("wss://", "https://").replace(
+                "ws://", "https://"
             )
         else:
-            origin = options.origin.replace("ws://", "http://")
+            origin = options.origin.replace("wss://", "https://").replace(
+                "ws://", "http://"
+            )
         reset_url = "%s/reset/token?u=%s&p=%s" % (origin, account, token)
         remote_ip = (
             self.request.headers.get("X-Real-IP")
