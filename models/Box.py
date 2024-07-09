7a29c8323f39b2899b5049595c25a633e78f7931
Brian Martin <bmartin5692@gmail.com>
2024-07-09 13:57:55 -0400

    export box order

--- models/Box.py
+++ models/Box.py
@@ -410,6 +410,7 @@ class Box(DatabaseObject):
         box_elem = ET.SubElement(parent, "box")
         box_elem.set("gamelevel", "%s" % str(self.game_level.number))
         ET.SubElement(box_elem, "name").text = self.name
+        ET.SubElement(box_elem, "order").text = self._order
         ET.SubElement(box_elem, "gamelevel").text = str(self.game_level.number)
         ET.SubElement(box_elem, "operatingsystem").text = self._operating_system
         ET.SubElement(box_elem, "description").text = self._description
