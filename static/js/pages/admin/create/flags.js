5d9b6f0c0a8870e7ff3b04a0170d2c2597d9a17d
Katie <kboscombe@student.bridgew.edu>
2025-05-10 23:04:02 -0400

    changed the string to 32 characters long, and made it so the random string is there by default but can be changed or randomly generated

--- static/js/pages/admin/create/flags.js
+++ static/js/pages/admin/create/flags.js
@@ -17,13 +17,17 @@ function getBoxFlags(box_uuid, flag_uuid) {
     }, 'json');
 }
 
-window.generateRandomString = function(length = 16) {
+window.generateRandomString = function(length = 32) {
     const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     let result = '';
     for (let i = 0; i < length; i++) {
         result += chars.charAt(Math.floor(Math.random() * chars.length));
     }
     document.getElementById('token').value = result;
+}
+
+window.onload = function (){
+    window.generateRandomString(32);
 }
 
 function testToken() {
